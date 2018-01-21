6b9e6191520a281a2f095d275d4f20974287b94a
Adam Reszka <adam.reszka85@gmail.com>
2018-01-21 14:15:27 +0100

    finally fixed parallax effect

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ $(document).ready(foxStart);
 
 function foxStart() {
   setHeight();
-  startParallax();
-
+  startParallax()
 }
 
 function setHeight() {
   var navHeight = $('nav').height();
   $('#sec1-main-view').height($(window).innerHeight()-navHeight);
 }
+
 function startParallax() {
   $('.parallax-window').parallax({imageSrc: 'images/red-fox-bg.jpg'});
 }
